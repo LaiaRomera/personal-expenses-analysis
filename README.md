93d4dac76897261be05b619ba35ba006588d2890
LaiaRom <romeramacias@gmail.com>
2026-01-27 10:06:10 +0100

    Basic version

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Personal Expenses Analysis
+Simple Python project to analyze monthly household expenses. 
+The goal of this project is to demonstrate basic data analysis skills, clean code structure and result visualization using personal data.
+This project loads expense data from a CSV file, performs basic data cleaning, analyzes total and category-based expenses, and visualizes the results.  
 
-Simple Python project to analyze household expenses.
+
+# Proyect Structure:
+personal-expenses-analysis/
+│
+├── data/
+│ └── expenses.csv #Raw expense data.
+│
+├── expenses_analysis.py #Main analysis script.
+├── requirements.txt #Project dependencies you may install.
+└── README.md
+
+# Clone the repository:
+git clone https://github.com/laiaRomera/personal-expenses-analysis.git
+
+
+# Autor 
+Laia Romera Macías  
+Junior Python & Data Analysis Student  
--- expenses_analysis.py
+++ expenses_analysis.py
@@ -1,18 +1,39 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("data/expenses.csv")
+def load_data(path):
+    """Load the CSV file."""
+    return pd.read_csv(path)
 
-print(df.head())
-print("Total expenses:", df["amount"].sum())
+def clean_data(df):
+    """Basic cleaning."""
+    df["amount"] = pd.to_numeric(df["amount"], errors="coerce") #converts to NaN if there is any problem.
+    df["date"] = pd.to_datetime(df["date"], errors="coerce")
+    df = df.dropna(subset=["amount", "date"])  #remove missing values.
+    return df
 
-expenses_by_category = df.groupby("category")["amount"].sum()
-print("\nExpenses by category:")
-print(expenses_by_category)
+def analyze_expenses(df):
+    """Calculate total expenses and expenses by category."""
+    total = df["amount"].sum()
+    by_category = df.groupby("category")["amount"].sum()
+    return total, by_category
 
-expenses_by_category.plot(kind="bar")
-plt.title("Expenses by Category")
-plt.xlabel("Category")
-plt.ylabel("Amount (€)")
-plt.tight_layout()
-plt.show()
+def graphic(expenses_by_category):
+    """graphic to view the expenses."""
+    expenses_by_category.plot(kind="bar", cmap = "cividis")
+    plt.title("Expenses by Category")
+    plt.xlabel("Category")
+    plt.ylabel("Amount (€)")
+    plt.tight_layout()
+    plt.show()
+
+def main():
+    df = load_data("data/expenses.csv")
+    total, expenses_by_category = analyze_expenses(df)
+    print(f"Total expenses: {total:.2f} €")
+    print("\nExpenses by category:")
+    print(expenses_by_category)
+    graphic(expenses_by_category)
+
+if __name__ == "__main__":
+    main()
